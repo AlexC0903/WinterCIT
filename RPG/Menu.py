a268a2b5de45db4b5e5f6bd13e913355644ea826
JeffreyYou55 <59158990+JeffreyYou55@users.noreply.github.com>
2019-12-24 12:39:41 +0900

    beautifiyTips

--- RPG/Menu.py
+++ RPG/Menu.py
@@ -3,26 +3,30 @@ import src
 
 pygame.init()
 
+
 def text_objects(text, font):
     textSurface = font.render(text, True, src.colors["black"])
     return textSurface, textSurface.get_rect()
 
+
 def button(index):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
-    buttonX = src.display["dwidth"] * (1/8)
-    buttonY = src.display["dheight"] * ((index*2+1)/7)
-    buttonW = src.display["dwidth"] * (6/8)
-    buttonH = src.display["dheight"] * (1/7)
+    buttonX = src.display["dwidth"] * (1 / 8)
+    buttonY = src.display["dheight"] * ((index * 2 + 1) / 7)
+    buttonW = src.display["dwidth"] * (6 / 8)
+    buttonH = src.display["dheight"] * (1 / 7)
 
-    pygame.draw.rect(src.gameDisplay, src.menu_buttons[index]["color"], (buttonX, buttonY
-    , buttonW , buttonH))
+    pygame.draw.rect(
+        src.gameDisplay, src.menu_buttons[index]["color"], (buttonX, buttonY, buttonW, buttonH))
     # Text
     smallText = pygame.font.Font("freesansbold.ttf", 20)
-    textSurf, textRect = text_objects(src.menu_buttons[index]["content"], smallText)
-    textRect.center = ((src.display["dwidth"] * (1/2)), src.display["dheight"] * ((index*4+3)/14))
+    textSurf, textRect = text_objects(
+        src.menu_buttons[index]["content"], smallText)
+    textRect.center = ((src.display["dwidth"] * (1 / 2)),
+                       src.display["dheight"] * ((index * 4 + 3) / 14))
     src.gameDisplay.blit(textSurf, textRect)
-    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+    if x + w > mouse[0] > x and y + h > mouse[1] > y:
         if click[0] == 1 and action != None:
             action()
 
